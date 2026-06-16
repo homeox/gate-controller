5e4ac1e95d2242bca31404af28f1864d98e350e2
Richard Edwards <r.kiwi.edwards@gmail.com>
2026-06-16 12:56:33 +1000

    Track Firebase web config for hosting

---
+++ gate-cloud/public/firebase-config.js
@@ -0,0 +1,10 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyCC-0al9LUyY89_lF2pmE1GHNP0mniBMJQ",
+  authDomain: "gate-controller-1b092.firebaseapp.com",
+  databaseURL: "https://gate-controller-1b092-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gate-controller-1b092",
+  storageBucket: "gate-controller-1b092.firebasestorage.app",
+  messagingSenderId: "965784927914",
+  appId: "1:965784927914:web:d443b08a175fe4e0959ad3",
+  measurementId: "G-X9ZT4X4QVG"
+};
